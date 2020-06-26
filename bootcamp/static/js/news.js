3503bb28c0302179d405916a848fd73e2ab3be35
Nicholas DiMaria <ndimaria@stevens.edu>
2020-06-26 13:40:09 -0400

    add error function to ajax

--- bootcamp/static/js/news.js
+++ bootcamp/static/js/news.js
@@ -119,7 +119,26 @@ $(function () {
           $(".like .heart", li).removeClass("fa fa-heart-o");
           $(".like .heart", li).addClass("fa fa-heart");
         }
-      }
+      },
+      error: function (jqXHR, exception) {
+        var msg = '';
+        if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+        } else if (jqXHR.status == 404) {
+            msg = 'Requested page not found. [404]';
+        } else if (jqXHR.status == 500) {
+            msg = 'Internal Server Error [500].';
+        } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+        } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+        } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+        } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+        }
+        console.log(msg);
+      },
     });
     return false;
   });
